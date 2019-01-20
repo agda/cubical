0e2d9069ed35e92b560018b4ea275334c2f66b67
ice1000 <ice1000kotlin@foxmail.com>
2019-01-20 11:59:52 -0500

    Add GenerateEverything.hs, remove hand-written everything modules

---
+++ GenerateEverything.hs
@@ -0,0 +1,131 @@
+{-# LANGUAGE PatternGuards #-}
+
+import qualified Data.List as List
+import Control.Applicative
+import System.Environment
+import System.IO
+import System.Exit
+import System.FilePath
+import System.FilePath.Find
+
+headerFile     = "Header"
+allOutputFile  = "Everything"
+srcDirs        = ["Cubical/HITs", "Cubical/Basics"]
+
+excludedModules :: [FilePath]
+excludedModules = map toAgdaFilePath
+  [ "Cubical.HITs.HInt"
+  , "Cubical.HITs.HitInt"
+  , "Cubical.HITs.Everything"
+  , "Cubical.Basics.Everything"
+  ] where
+
+  toAgdaFilePath :: String -> FilePath
+  toAgdaFilePath name = concat
+    [ map (\ c -> if c == '.' then '/' else c) name
+    , ".agda"
+    ]
+
+main = do
+  args <- getArgs
+  case args of
+    [] -> return ()
+    _  -> hPutStr stderr usage >> exitFailure
+
+  header <- readFileUTF8 headerFile
+  mapM_ (generateFor header) srcDirs
+
+generateFor :: String -> FilePath -> IO ()
+generateFor header srcDir = do
+  modules <- filter isLibraryModule . List.sort <$>
+               find always
+                    (extension ==? ".agda" ||? extension ==? ".lagda")
+                    srcDir
+  headers <- mapM extractHeader modules
+
+  let mkModule str = "module " ++ str ++ " where"
+  let content      = zip modules headers
+  let fileNoExt    = srcDir ++ "/" ++ allOutputFile
+
+  writeFileUTF8 (fileNoExt ++ ".agda") $
+    unlines [ header
+            , mkModule $ map (\c -> if c == '/' then '.' else c) fileNoExt
+            , format $ filter ((`notElem` excludedModules) . fst) content
+            ]
+
+-- | Usage info.
+
+usage :: String
+usage = unlines
+  [ "GenerateEverything: A utility program for Agda's cubical library."
+  , ""
+  , "Usage: runghc GenerateEverything.hs"
+  , ""
+  , "This program should be run in the base directory of a clean checkout of"
+  , "the library."
+  , ""
+  , "The program generates documentation for the library by extracting"
+  , "headers from library modules. The output is written to " ++ allOutputFile
+  , "with the file " ++ headerFile ++ " inserted verbatim at the beginning."
+  ]
+
+-- | Returns 'True' for all Agda files except for core modules.
+
+isLibraryModule :: FilePath -> Bool
+isLibraryModule f =
+  takeExtension f `elem` [".agda", ".lagda"]
+  -- && dropExtension (takeFileName f) /= "Core"
+
+-- | Reads a module and extracts the header.
+
+extractHeader :: FilePath -> IO [String]
+extractHeader mod = fmap (extract . lines) $ readFileUTF8 mod
+  where
+  delimiter = all (== '-')
+
+  extract (d1 : "-- Experimental library for Cubical Agda" : "--" : ss)
+    | delimiter d1
+    , (info, d2 : rest) <- span ("-- " `List.isPrefixOf`) ss
+    , delimiter d2
+    = info
+  extract (d1 : _)
+    | not (delimiter d1)
+    , last d1 == '\r'
+    = error $ mod ++ " contains \\r, probably due to git misconfiguration; maybe set autocrf to input?"
+  extract _ = [] -- error $ mod ++ " is malformed. It is required to have a module header. Please see other existing files or consult the agda-stdlib authors."
+
+-- | Formats the extracted module information.
+
+format :: [(FilePath, [String])]
+          -- ^ Pairs of module names and headers. All lines in the
+          -- headers are already prefixed with \"-- \".
+       -> String
+format = unlines . concat . map fmt
+  where
+  fmt (mod, header) = "" : header ++ ["import " ++ fileToMod mod]
+
+-- | Translates a file name to the corresponding module name. It is
+-- assumed that the file name corresponds to an Agda module under
+-- 'srcDir'.
+
+fileToMod :: FilePath -> String
+fileToMod = map slashToDot . dropExtension . makeRelative "."
+  where
+  slashToDot c | isPathSeparator c = '.'
+               | otherwise         = c
+
+-- | A variant of 'readFile' which uses the 'utf8' encoding.
+
+readFileUTF8 :: FilePath -> IO String
+readFileUTF8 f = do
+  h <- openFile f ReadMode
+  hSetEncoding h utf8
+  s <- hGetContents h
+  length s `seq` return s
+
+-- | A variant of 'writeFile' which uses the 'utf8' encoding.
+
+writeFileUTF8 :: FilePath -> String -> IO ()
+writeFileUTF8 f s = withFile f WriteMode $ \h -> do
+  hSetEncoding h utf8
+  hPutStr h s
