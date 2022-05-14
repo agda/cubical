055d842e8e4b713823a6f59f068de2f1f7c2f70f
Guilherme <guilhermehas@hotmail.com>
2022-05-14 19:57:43 -0300

    added installPhase

--- flake.nix
+++ flake.nix
@@ -6,17 +6,34 @@
   outputs = { self, flake-utils, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
     let pkgs = import nixpkgs { inherit system; };
+        everythingFile = "./Everything.agda";
+        extensions = [
+          "agda"
+          "agda-lib"
+          "agdai"
+          "lagda"
+          "lagda.md"
+          "lagda.org"
+          "lagda.rst"
+          "lagda.tex"
+        ];
     in rec {
       packages.cubical = with pkgs;
         stdenv.mkDerivation {
           name = "cubical";
           src = ./.;
           LC_ALL = "en_US.UTF-8";
+          preConfigure = ''export AGDA_EXEC=agda'';
 
           # The cubical library has several `Everything.agda` files, which are
           # compiled through the make file they provide.
           nativeBuildInputs = [ agda ghc glibcLocales ];
-          install = "touch $out";
+
+          installPhase = ''
+            mkdir -p $out
+            find -not \( -path ${everythingFile} \) -and \( ${lib.concatMapStringsSep " -or " (p: "-name '*.${p}'") extensions} \) -exec cp -p --parents -t "$out" {} +
+          '';
+
         };
       defaultPackage = packages.cubical;
     });
