b00fda5971d3f2e49bbe4e54b1f1863320aaa786
Guilherme <guilhermehas@hotmail.com>
2022-05-14 18:12:13 -0300

    added nix flake files

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Cubical Agda";
+
+  inputs.flake-utils.url = github:numtide/flake-utils;
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = import nixpkgs { inherit system; };
+    in rec {
+      packages.cubical = with pkgs;
+        stdenv.mkDerivation {
+          name = "cubical";
+          src = ./.;
+          LC_ALL = "en_US.UTF-8";
+
+          # The cubical library has several `Everything.agda` files, which are
+          # compiled through the make file they provide.
+          nativeBuildInputs = [ agda ghc glibcLocales ];
+        };
+      defaultPackage = packages.cubical;
+    });
+}
