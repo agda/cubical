7039feccf434861d16921522ebdf1bd2c3de5306
Guilherme <guilhermehas@hotmail.com>
2022-06-30 17:19:45 -0300

    changed cubical derivation

--- flake.nix
+++ flake.nix
@@ -6,35 +6,27 @@
   outputs = { self, flake-utils, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
     let pkgs = import nixpkgs { inherit system; };
-        everythingFile = "./Everything.agda";
-        extensions = [
-          "agda"
-          "agda-lib"
-          "agdai"
-          "lagda"
-          "lagda.md"
-          "lagda.org"
-          "lagda.rst"
-          "lagda.tex"
-        ];
+        cubical = with pkgs;
+          agdaPackages.mkDerivation rec {
+            pname = "cubical";
+            version = "0.4";
+
+            src = ./.;
+
+            LC_ALL = "en_US.UTF-8";
+
+            preConfigure = ''export AGDA_EXEC=agda'';
+
+            # The cubical library has several `Everything.agda` files, which are
+            # compiled through the make file they provide.
+            nativeBuildInputs = [ ghc glibcLocales ];
+            buildPhase = ''
+              make
+            '';
+            meta = {};
+          };
     in rec {
-      packages.cubical = with pkgs;
-        stdenv.mkDerivation {
-          name = "cubical";
-          src = ./.;
-          LC_ALL = "en_US.UTF-8";
-          preConfigure = ''export AGDA_EXEC=agda'';
-
-          # The cubical library has several `Everything.agda` files, which are
-          # compiled through the make file they provide.
-          nativeBuildInputs = [ agda ghc glibcLocales ];
-
-          installPhase = ''
-            mkdir -p $out
-            find -not \( -path ${everythingFile} \) -and \( ${lib.concatMapStringsSep " -or " (p: "-name '*.${p}'") extensions} \) -exec cp -p --parents -t "$out" {} +
-          '';
-
-        };
-      defaultPackage = packages.cubical;
+      packages.cubical = cubical;
+      defaultPackage = cubical;
     });
 }
