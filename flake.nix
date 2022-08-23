f7deb8800001af63dd6cd810004ac82efb5137e9
Guilherme <guilhermehas@hotmail.com>
2022-08-23 18:55:12 -0300

    updated flake.nix

--- flake.nix
+++ flake.nix
@@ -5,42 +5,46 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, flake-utils, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system:
-    let pkgs = nixpkgs.legacyPackages.${system};
-        cubical = with pkgs;
-          agdaPackages.mkDerivation rec {
-            pname = "cubical";
-            version = "0.4";
+    let
+      inherit (nixpkgs.lib) cleanSourceWith hasSuffix;
+      overlay = final: prev: {
+        cubical = final.agdaPackages.mkDerivation rec {
+          pname = "cubical";
+          version = "0.4";
 
-            src = pkgs.lib.cleanSourceWith {
-              filter = name: type:
-                !(pkgs.lib.hasSuffix ".nix" name)
-              ;
-              src = ./.;
-            };
-
-
-            LC_ALL = "C.UTF-8";
-
-            preConfigure = ''export AGDA_EXEC=agda'';
-
-            # The cubical library has several `Everything.agda` files, which are
-            # compiled through the make file they provide.
-            nativeBuildInputs = [ ghc ];
-            buildPhase = ''
-              make
-            '';
-            meta = {
-              description = "An experimental library for Cubical Agda";
-              homepage = "https://github.com/agda/cubical";
-              license = "MIT License";
-            };
+          src = cleanSourceWith {
+            filter = name: type:
+              !(hasSuffix ".nix" name)
+            ;
+            src = ./.;
           };
-    in rec {
-      packages = {
-        cubical = cubical;
-        agdaWithCubical = pkgs.agda.withPackages [cubical];
-        default = cubical;
+
+
+          LC_ALL = "C.UTF-8";
+
+          preConfigure = ''export AGDA_EXEC=agda'';
+
+          # The cubical library has several `Everything.agda` files, which are
+          # compiled through the make file they provide.
+          nativeBuildInputs = [ final.ghc ];
+          buildPhase = ''
+            make
+          '';
+          meta = {
+            description = "An experimental library for Cubical Agda";
+            homepage = "https://github.com/agda/cubical";
+            license = "MIT License";
+          };
+        };
+        agdaWithCubical = final.agdaPackages.agda.withPackages [final.cubical];
       };
-    });
+    in
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; overlays = [ overlay ]; };
+      in rec {
+        packages = with pkgs; rec {
+          inherit cubical agdaWithCubical;
+          default = cubical;
+        };
+      });
 }
